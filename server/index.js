d7dfc4d18d4bf1fd2c276c440c2d7d052ba527d2
arunkumar198857 <arunkumar198857@gmail.com>
2021-01-07 19:24:45 +0530

    added cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const cors = require('cors');
 
 const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
 
@@ -44,5 +45,6 @@ io.on('connection', (socket) => {
 });
 
 app.use(router);
+app.use(cors());
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
